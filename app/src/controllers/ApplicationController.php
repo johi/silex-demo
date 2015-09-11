30e73f13c9725ba9a0e8a1b2ee2a8a136d973558
johi <johan.schulz@gmail.com>
2015-09-11 19:49:50 +0200

    the app so far

---
+++ app/src/controllers/ApplicationController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace silex-demo\subscriptionform\app\controller;
+
+use Monolog\Handler\StreamHandler;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Monolog\Logger;
+
+class ApplicationController
+{
+
+    const MAX_NAME_LENGTH = 255;
+    const MIN_NAME_LENGTH = 5;
+
+    /**
+     * @param Application $application
+     * @param Request $request
+     * @return mixed
+     */
+    public function subscribe(Application $application, Request $request)
+    {
+        $form = $application['form.factory']->createBuilder('form')
+            ->add('name', 'text', array(
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                    new Assert\Length(array('min' => self::MIN_NAME_LENGTH, 'max' => self::MAX_NAME_LENGTH))
+                ),
+                'attr' => array('class' => 'form-control', 'placeholder' => 'Dit navn')
+            ))
+            ->add('email', 'email', array(
+                'constraints' => array(new Assert\Email()),
+                'attr' => array('class' => 'form-control', 'placeholder' => 'Din e-mail adresse')
+            ))
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $subscriber = $form->getData();
+            #emailing a confirmation message
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Confirmation of your subscription')
+                ->setFrom(array('noreply@example.com' => 'Subscription Service'))
+                ->setTo($subscriber['email'])
+                ->setBody(sprintf('Dear %s, this is a confirmation that you are now subscribe...', $subscriber['name']));
+            $application['mailer']->send($message);
+            #logging the subscription
+            #should be directly translatable to a MonologServiceProvider using Silex's built in service
+            $logger = new Logger('Subscription');
+            $logger->pushHandler(new StreamHandler(__DIR__ . '/../../log/subscription.log', Logger::INFO));
+            $logger->addInfo(sprintf("User '%s' subscribed with email '%s'.", $subscriber['name'],
+                $subscriber['email']));
+            #making a subquery
+            $subRequest = Request::create('/summary', 'GET', $subscriber);
+            $application['session']->set('subscriber', $subscriber);
+            return $application->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
+        }
+        return $application['twig']->render('subscribe.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @param Application $application
+     * @param Request $request
+     * @return mixed
+     */
+    public function summary(Application $application, Request $request)
+    {
+        $subscriber = $application['session']->get('subscriber');
+        return $application['twig']->render('summary.twig', array('subscriber' => $subscriber));
+    }
+}
---
+++ app/src/web/index.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace dr\subscriptionform\app\web;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../src/controllers/ApplicationController.php';
+
+use Silex\Application;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\SwiftmailerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+use dr\subscriptionform\app\controller\ApplicationController;
+
+$application = new Application();
+$application['debug'] = true;
+$application->register(new ServiceControllerServiceProvider());
+$application->register(new FormServiceProvider());
+$application->register(new ValidatorServiceProvider());
+$application->register(new TranslationServiceProvider(), array(
+    'locale_fallbacks' => array('en'),
+));
+$application->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../../src/views',
+    'twig.form.templates' => array('/Form/form_widget.twig')
+));
+$application->register(new SessionServiceProvider());
+$application->register(new SwiftmailerServiceProvider());
+
+//$app->register(new Provider\UrlGeneratorServiceProvider(), array());
+$application->register(new HttpFragmentServiceProvider());
+$application['application.controller'] = $application->share(function () use ($application) {
+    return new ApplicationController();
+});
+$application->match('/', 'application.controller:subscribe');
+$application->get('/summary', 'application.controller:summary');
+$application['swiftmailer.options'] = array(
+    'host' => 'localhost',
+    'port' => '1025',
+    'username' => '',
+    'password' => '',
+    'encryption' => null,
+    'auth_mode' => null
+);
+$application->run();
